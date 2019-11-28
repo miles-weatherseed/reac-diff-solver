734929512eadef447c19c5fefa79d9357e2c2016
barnum <barnum.swannell@worc.ox.ac.uk>
2019-11-28 15:50:55 +0000

    extra tests started

---
+++ test_solver_setup.py
@@ -0,0 +1,38 @@
+import unittest
+import numpy as np
+from solver import Solver
+
+class TestModel(unittest.TestCase):
+    xbounds = [0.0, 2.0]
+    ybounds = [0.0, 2.0]
+    gridsize = 0.5
+
+    def initial_function(self, X, Y):
+        return np.sin(4 * np.pi * X) * np.sin(2 * np.pi * Y) #* np.exp(-40 * np.pi ** 2 * T)
+
+    def exact_solution(self, X, Y, T):
+        return np.sin(4 * np.pi * X) * np.sin(2 * np.pi * Y) * np.exp(-40 * np.pi ** 2 * T)
+
+    def u_reaction(self, u, v):
+        return 0
+
+    def v_reaction(self, u, v)
+
+    def setUp(self) -> None:
+        self.sol = Solver(self.xbounds, self.ybounds, self.gridsize)
+
+    def test_init(self):
+        self.assertEqual(self.sol.xBounds, self.xbounds)
+        self.assertEqual(self.sol.yBounds, self.ybounds)
+
+    def test_set_bounds(self):
+        xbounds = [10, 20]
+        self.sol.set_xBounds(xbounds)
+        self.assertEqual(self.sol.xBounds, xbounds)
+
+        ybounds = [10, 20]
+        self.sol.set_yBounds(ybounds)
+        self.assertEqual(self.sol.yBounds, ybounds)
+
+
+
