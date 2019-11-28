02fcd8b208180b34780456644b924730aa16781c
Muriel van der Laan <muriel.vanderlaan@wolfson.ox.ac.uk>
2019-11-28 17:06:38 +0000

    fixed many stupid bugs

--- test_solver.py
+++ test_solver.py
@@ -9,15 +9,15 @@ def returns_zero(u,v):
 def initial_conditions(X,Y):
     return [np.sin(4*np.pi*X) * np.sin(2*np.pi*Y), np.sin(4*np.pi*X) * np.sin(2*np.pi*Y)]
 
-solver = Solver([0.0,1.0], [0.0,1.0], 50, initial_conditions)
-solver.solve([0.1,0.2,0.4],[.5,1.0])
+solver = Solver([0.0,1.0], [0.0,1.0], 200, initial_conditions)
+t = np.array([0.0,0.1])
+u,v = solver.solve(t,[.5,1.0])
 
-t = np.array([0.0,0.1,0.2,0.4])
 T,Y,X = np.meshgrid(t,solver.y,solver.x, indexing = 'ij')
 solution = np.sin(4*np.pi*X) * np.sin(2*np.pi*Y) * np.exp(-20*np.pi**2 * T)
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-ax.plot_surface(solver.X,solver.Y,solver.uSolution[1])
+ax.plot_surface(solver.X,solver.Y,v[1])
 ax.plot_surface(solver.X,solver.Y,solution[1])
 plt.show()
