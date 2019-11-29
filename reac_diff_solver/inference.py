104a5430f0dac2e5e1caca52aba3655376856ba9
Miles Weatherseed <weatherseed@maths.ox.ac.uk>
2019-11-29 16:20:43 +0000

    More thorough docstrings

--- reac_diff_solver/inference.py
+++ reac_diff_solver/inference.py
@@ -4,34 +4,60 @@ import numpy as np
 
 '''
 We can take a time series which we believe to have been obtained from a reaction diffusion PDE system and run this
-inference class on it. The output will be the parameters
+inference class on it. The output will be the parameters that minimize some cost function defined as L2 error.
 '''
 
 class Inference(Solver):
     def __init__(self, u_data, v_data, times):
         '''
-        Takes an n dimensional vector of times with corresponding observed data for u and v in (n, x, x) arrays
-        where x is the dimension of the mesh. Initalizes these in the solver.
-        :param data: the observed data we wish to infer the parameters from
-        :param times: the corresponding times
+        Initializes our inference class by receiving the observed grids of u and v values and the times in the time
+        series. We set the x and y boundaries and the initial conditions separately.
+
+        :param u_data: observed values of u at each point in the grid at each time in the time series
+        :type u_data: n x gridsize x gridsize numpy array
+        :param u_data: observed values of v at each point in the grid at each time in the time series
+        :type u_data: n x gridsize x gridsize numpy array
+        :param times: array of n evenly-spaced times
+        :type times: 1-dimensional numpy array
         '''
+
         self.u_data = u_data
         self.v_data = v_data
         self.times = times
 
-    def set_model(self, xBounds, yBounds, gridsize, initial_conditions_function):
-        self.solver = Solver(xBounds, yBounds, gridsize, initial_conditions_function)
+        self.gridsize = u_data.shape[1] #infer the gridsize from this dimension
+
+    def set_model(self, xBounds, yBounds, initial_conditions_function):
+        '''
+        Sets up the reaction diffusion model we wish to consider. We determine the x and y boundaries for consideration
+        and provide some initial conditions for our system.
+
+        :param xBounds: x-range of the problem
+        :type u_data: list of 2 floats
+        :param yBounds: y-range of the problem
+        :type u_data: list of 2 floats
+        :param initial_condition_function: calculates the values of u and v at t=0 at each gridpoint
+        :type initial_condition_function: function that takes two 2d numpy arrays and returns a list of two 2d numpy arrays
+        '''
+
+        self.solver = Solver(xBounds, yBounds, self.gridsize, initial_conditions_function)
 
     def set_reaction_function(self, function):
+        '''
+        This is optional. We can choose to provide a nonlinear term to our system.
+        :param function: calculates the value of the reaction terms at the given u and v
+        :type function: function that takes two numpy arrays (containing values of u and v) and a list of parameters
+        and returns a list of two numpy arrays
+        '''
+
         self.solver.set_reactionFunction(function)
 
     def error_func(self):
         '''
         Returns L2 error between output using current proposed set of parameters and the observed data
-        :param u_output:
-        :param v_output:
-        :return:
+        :return tot_error: the total sum of L2 errors at each timestep between the true u,v values and the solved values
         '''
+
         tot_error = 0
         for i in range(len(self.times)):
             u_diff_mat = self.u_data[i, :, :] - self.u_output[i, :, :]
@@ -46,6 +72,7 @@ class Inference(Solver):
         :param parametervalues: the proposed set of parameter values at this point in the optimization
         :return: the L2 error with these parameters
         '''
+
         self.u_output, self.v_output = self.solver.solve(self.times, parametervalues)
         return self.error_func()
 
@@ -56,6 +83,7 @@ class Inference(Solver):
         :param x0: the initial estimate of parameters
         :return best_fit_parameters: a vector of the fitted parameters
         '''
+        
         res = optimize.minimize(self.cost_func, x0, method='Nelder-Mead')
         best_fit_parameters = res.x
         return best_fit_parameters
